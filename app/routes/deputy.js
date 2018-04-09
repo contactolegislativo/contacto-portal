179fe7f6cad01f3138ccad2e2d0117eeb6492c46
Cristian Colorado <cristian.colorado@hbr.org>
2018-04-09 13:15:57 -0500

    Fix: Slug deputy sending to wrong ejs

--- app/routes/deputy.js
+++ app/routes/deputy.js
@@ -76,23 +76,27 @@ var urlParamsValidatorCirc = function(req, res, next) {
 /* /diputado/<estado>/distrito/<no> */
 router.get('/:state/distrito/:id', urlParamsValidator, (req, res, next) => {
 	let queryString =
+    'select * from ProfileDetails where state = :stateName and district = :district and type = :type order by latestAttendance desc'
   	'select * from Seats s join Deputies d on s.id = d.SeatId where s.state = :stateName and s.area = :district and s.curul is null order by d.latestAttendance desc';
 
   models.sequelize
 	  .query(queryString, {
 	    replacements: {
 				stateName: cache[req.params.state].name,
-				district: req.params.id
+				district: req.params.id,
+        type: 'Mayoría Relativa'
 			},
 	    type: models.sequelize.QueryTypes.SELECT
 	  })
 	  .then(function(deputies) {
-			res.render('index', {
-		  		title: renderTitle(deputies[0]),
-					deputy: deputies[0],
-					alternate: deputies[1],
-					host: req.headers.host
-				});
+      let deputy = deputies[0];
+      let alternates = deputies.filter(item => deputy.slug !== item.slug) || { displayName: 'Ninguno', attendances: 0 };
+      res.render('deputy', {
+          title: renderTitle(deputy),
+          deputy: deputy,
+          alternates: alternates,
+          host: req.headers.host
+        });
 	  }, function(err) {
 			console.log(err);
 			renderError(res, `No se pudo encontrar informacion para el distrito  ${req.params.id} de ${cache[req.params.state].name}.`);
@@ -102,7 +106,8 @@ router.get('/:state/distrito/:id', urlParamsValidator, (req, res, next) => {
 /* /legislatura/LXIII/diputado/{state}/circunscripcion/{ditrict}/{id} */
 router.get('/:state/circunscripcion/:districtId/:id', urlParamsValidatorCirc, (req, res, next) => {
 	let queryString =
-  	'select * from Seats s join Deputies d on s.id = d.SeatId where s.state = :stateName and s.area = :district and s.id = :id order by d.latestAttendance desc';
+    'select * from ProfileDetails where state = :stateName and id = :id and district = :district order by latestAttendance desc';
+  	// 'select * from Seats s join Deputies d on s.id = d.SeatId where s.state = :stateName and s.area = :district and s.id = :id order by d.latestAttendance desc';
 
   models.sequelize
 	  .query(queryString, {
@@ -117,10 +122,12 @@ router.get('/:state/circunscripcion/:districtId/:id', urlParamsValidatorCirc, (r
 			if(deputies.length === 0) {
 				renderError(res, `El contenido no esta disponible`);
 			} else {
-				res.render('index', {
-			  		title: renderTitle(deputies[0]),
-						deputy: deputies[0],
-						alternate: deputies[1],
+        let deputy = deputies[0];
+        let alternates = deputies.filter(item => deputy.slug !== item.slug) || { displayName: 'Ninguno', attendances: 0 };
+				res.render('deputy', {
+			  		title: renderTitle(deputy),
+						deputy: deputy,
+						alternates: alternates,
 						host: req.headers.host
 					});
 
@@ -133,11 +140,11 @@ router.get('/:state/circunscripcion/:districtId/:id', urlParamsValidatorCirc, (r
 
 /* /diputado/<slug> */
 router.get('/:slug', (req, res, next) => {
-	let queryString =
-  	'select * from Seats s join Deputies d on s.id = d.SeatId where s.id in ( select SeatId from Deputies d where d.slug = :slug ) order by d.latestAttendance desc';
+  let queryString =
+    'select * from ProfileDetails where id in (select id from ProfileDetails where slug = :slug)';
 
 	let slug = normalize(decodeURIComponent(req.params.slug));
-
+console.log('slug:', slug)
   models.sequelize
 	  .query(queryString, {
 	    replacements: {
